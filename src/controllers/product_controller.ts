3d7b69205b4d9746e74bad2404484dde9f7655a8
Garica Gabriel <garcia-gabriel-246@hotmail.com>
2024-05-08 20:54:11 -0300

    Added: Controllers & 1/2 Routes

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "express";
+import { Request, Response, query } from "express";
+import prisma from "../models/prisma_model";
 
 //AGREGAR A CARRITO
 export const agregarAlCarrito= async(req: Request, res: Response): Promise<void> =>{
     const {userId, productId, quantity} = req.body;
- 
+
     const producto = await fetch(`https://dummyjson.com/products/${productId}`).then(res => res.json())
 
     if(!producto.id){
@@ -11,5 +12,49 @@ export const agregarAlCarrito= async(req: Request, res: Response): Promise<void>
         return
     }
 
-    const cartItem = await prisma.create
+    const cartItem = await prisma.cartItem.create({
+        data: {
+            userId,
+            productId,
+            quantity,
+        },
+    })
+
+    res.status(201).json(cartItem)
+}
+
+//ACTUALIZAR CANTIDAD DE ITEMS DE CARRITO
+export const actualizarCantidad= async(req: Request, res: Response): Promise<void> =>{
+    const {paramId} = req.params;
+    const {quantity} = req.body;
+    const cartItemId= Number(paramId)
+
+    const cartItem = await prisma.cartItem.update({
+        where: { id: cartItemId },
+        data: { quantity },
+    })
+
+    res.status(200).json(cartItem)
+}
+
+//ELIMINAR UN ITEM DEL CARRITO
+export const eliminarDelCarrito= async(req:Request, res:Response): Promise<void> => {
+    const paramsId= req.params;
+    const cartItemId= Number(paramsId)
+
+    await prisma.cartItem.delete({
+        where: {id: cartItemId}
+    })
+
+    res.status(200).json()
+}
+
+//OBTENER EL CARRITO DEL USUARIO
+export const obtenerCarrito= async(req:Request, res:Response): Promise<void> =>{
+    const queryId= req.query;
+    const userId= Number(queryId);
+
+    const cartItems = await prisma.cartItem.findMany({
+        where: {userId},
+    })
 }
---
+++ src/routes/product.routes.ts
@@ -0,0 +1,10 @@
+import {agregarAlCarrito, actualizarCantidad, eliminarDelCarrito, obtenerCarrito} from '../controllers/product_controller'
+import express from 'express';
+
+const router= express.Router();
+
+//Rutas
+
+
+
+
